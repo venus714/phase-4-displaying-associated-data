6bdef9fb6eacf2f0717219cbad24b1077cb5f368
Student <venus.kabera@student.moringaschool.com>
2023-03-16 10:35:09 +0300

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :do_house
   end
 
 end
